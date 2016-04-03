9b4898a27c967fe7b04d80143edd96afd119e4f2
kaustuv deolal <vutsuak96@gmail.com>
2016-04-03 19:35:05 +0530

    size and time functions added

--- script.py
+++ script.py
@@ -1,10 +1,27 @@
+__name__ = "vutsuak"
+
 import os
 import time
 
-f=open("C:\Users\windows 7\Desktop\Reading Between The Lines.docx")
 
-#print (os.stat("C:\Users\windows 7\Desktop\Reading Between The Lines.docx"))
-x= list(os.stat("C:\Users\windows 7\Desktop\Reading Between The Lines.docx"))
-print x[6]*.001
-print time.localtime(x[7])
-print time.localtime(x[8])
+def file_size(l):
+    return str(l[6] * .001) + " KB"
+
+
+def modification_time(l):
+    return time.ctime(l[7])
+
+
+def metadata_change_time(l):
+    return time.ctime(l[9])
+
+
+def filestat(path):
+    l = list(os.stat(path))
+    print file_size(l)
+    print "last modified on " + str(modification_time(l))
+    print "last change in metadata " + str(metadata_change_time(l))
+
+
+if __name__ == "vutsuak":
+    filestat("C:\Users\windows 7\Desktop\Reading Between The Lines.docx")
