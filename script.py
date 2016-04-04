9f31a9a8f962747610fa29a3efe3a8114460fcdd
kaustuv deolal <vutsuak96@gmail.com>
2016-04-04 14:10:24 +0530

    added the word count function

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import time
 
 
 def file_size(l):
-    return str(l[6] * .001) + " KB"
+    return "SIZE IS: "+str(l[6] * .001) + " KB"
 
 
 def modification_time(l):
@@ -15,13 +15,17 @@ def modification_time(l):
 def metadata_change_time(l):
     return time.ctime(l[9])
 
+def word_count(path):
+    f=open(path)
+    return len(f.read())
+
 
 def filestat(path):
     l = list(os.stat(path))
     print file_size(l)
-    print "last modified on " + str(modification_time(l))
-    print "last change in metadata " + str(metadata_change_time(l))
-
+    print "LAST MODIFIED ON: " + str(modification_time(l))
+    print "LAST CHANGE IN METADATA: " + str(metadata_change_time(l))
+    print "THE WORD COUNT IS "+str(word_count(path))
 
 if __name__ == "vutsuak":
     filestat("C:\Users\windows 7\Desktop\Reading Between The Lines.docx")
