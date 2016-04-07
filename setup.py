75fd6852adad794e8aad7508d8cb4ba5fbde1226
kaustuv deolal <vutsuak96@gmail.com>
2016-04-07 20:33:03 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='filestats',
+      version='0.1',
+      description='Contains functions that return the stats associated with the file ',
+      url='https://github.com/Vutsuak16/filestats',
+      author='vutsuak',
+      author_email='vutsuak96@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
